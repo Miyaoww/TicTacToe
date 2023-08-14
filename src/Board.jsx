aa1c071a43b2b250a008e2e0dbaa94e7534256e8
Miyaow <miyaow.id@gmail.com>
2023-08-14 22:34:06 +0700

    First Commit

---
+++ src/Board.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('x');
+  }
+
+  return <button className='square' onClick={handleClick}>{value}</button>
+}
+
+export default  function Board() {
+  return (
+    <div className='board'>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div> 
+  )
+}
+
+
